07ab656c49eaab68861b9648e00e5a37dc608809
georgiosanagnostopoulos-cloud <georgios.anagnostopoulos@richmond.edu>
2024-12-09 22:38:41 -0500

    lol

--- Makefile
+++
@@ -1,7 +0,0 @@
-all: main
-
-main: main.cpp Timer.cpp SessionLog.cpp User.cpp ConfigManager.cpp
-	g++ -o main main.cpp Timer.cpp SessionLog.cpp User.cpp ConfigManager.cpp -lnlohmann_json
-
-clean:
-	rm -f main
--- SessionLog.cpp
+++
@@ -1,9 +0,0 @@
-#include "SessionLog.h"
-
-void SessionLog::logSession(const std::string& sessionType, int duration, const std::string& timestamp) {
-    logs.push_back({{"type", sessionType}, {"duration", duration}, {"timestamp", timestamp}});
-}
-
-std::vector<nlohmann::json> SessionLog::getSessionHistory() const {
-    return logs;
-}
